41d61d803caa914d5f63abd92053907757c60840
EvapDeveloper <aliraza@evap.my>
2022-03-12 14:52:16 +0500

    initialize commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import DeckGL from "@deck.gl/react";
+import { LineLayer } from "@deck.gl/layers";
+import Map from "react-map-gl";
+import { MapView, FirstPersonView } from "@deck.gl/core";
+
+// Set your mapbox access token here
+const MAPBOX_ACCESS_TOKEN =
+  "pk.eyJ1IjoiYWxpcmF6YXNoYXJhZmF0IiwiYSI6ImNsMG5oZ3JneTBhY20zY216Z21pN2Z2NWUifQ.5Zyn9fGJcoW1jS0a-K6-_A";
+
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+  longitude: -122.41669,
+  latitude: 37.7853,
+  zoom: 15,
+  pitch: 0,
+  bearing: 0,
+};
+
+// Data to be used by the LineLayer
+const data = [
+  {
+    sourcePosition: [-122.41669, 37.7853],
+    targetPosition: [-122.41669, 37.781],
+  },
+];
 
 function App() {
+  const layers = [new LineLayer({ id: "line-layer", data })];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DeckGL
+      initialViewState={INITIAL_VIEW_STATE}
+      controller={true}
+      layers={layers}
+    >
+      <MapView id="map" width="50%" controller={true}>
+        <Map
+          mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+          initialViewState={{
+            longitude: -100,
+            latitude: 40,
+            zoom: 3.5,
+          }}
+          style={{ width: 600, height: 400 }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+        />
+      </MapView>
+      <FirstPersonView width="50%" x="50%" fovy={50} />
+    </DeckGL>
   );
 }
 
